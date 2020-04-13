710f16e36b1ce0559fd1bd73a469fb800365f9cd
openxw <openxw@qq.com>
2020-04-13 10:59:13 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
+use App\Models\Status;
 
 class StaticPagesController extends Controller
 {
     //
     public function home()
     {
-        return view('static_pages.home');
+        $feed_items = [];
+        if (Auth::check()){
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+
+        return view('static_pages.home', compact('feed_items'));
     }
 
     public function help()
